6b18e1ff3d4ee81f80b31720a4ed18e20e1ac3f9
shijie <shijiechn@aliyun.com>
2022-05-02 17:49:30 +0800

    add weekend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,8 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
+
     
 </div>
     
@@ -15,6 +17,7 @@ import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
+import HomeWeekend from './components/Weekend.vue'
 
 
 export default {
@@ -23,7 +26,8 @@ export default {
         'home-header': HomeHeader,
         'home-swiper': HomeSwiper,
         'home-icons': HomeIcons,
-        'home-recommend': HomeRecommend
+        'home-recommend': HomeRecommend,
+        'home-weekend': HomeWeekend
     }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,104 @@
+<template>
+<div>
+    <div class="recommend-title">
+      周末去哪儿
+    </div>
+    <ul>
+        <router-link
+         tag="li"
+         class="list-item"
+         v-for="item in recommendList"
+         :key="item.id"
+         to="/detail">
+            <div class="item-img">
+                <img :src="item.imgUrl" alt="">
+            </div>
+            <div class="item-info">
+                <div class="info-title">{{item.title}}</div>
+                <div class="info-comment">{{item.comment}}</div>
+            </div>
+        </router-link>
+    </ul>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+  data () {
+    return {
+        recommendList: [
+            {
+                id: '001',
+                imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+                title: '清远周边逛',
+                comment: '人人都有一双发现美的眼睛，身边的事物最熟悉不过，那你可曾发现了被人遗漏的美？'
+            },
+            {
+                id: '002',
+                imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/f8/29dfa785277839.jpg_r_640x214_7d051523.jpg',
+                title: '阳江温泉知多少',
+                comment: '这里的天空和大海让人心旷神怡，配上舒缓的优美音乐，泡个暖暖的温泉岂不美哉'
+            },
+            {
+                id: '003',
+                imgUrl: 'https://imgs.qunarzz.com/sight/source/1812/47/cbdc44f461db0d.jpg_r_640x214_aab1da76.jpg',
+                title: '珠江夜游',
+                comment: '绚烂的霓虹将珠江照亮，跳上一艘游船，沉浸在这醉人的夜色中'
+            }
+        ]
+    };
+  },
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+@import '~styles/mixins.styl'
+
+div{
+    box-sizing: border-box;
+}
+.recommend-title{
+    width: 100%;
+    height: .8rem;
+    line-height: .8rem;
+    font-size: .28rem;
+    background-color: #eee;
+    padding: 0 .2rem; 
+}
+
+
+
+.list-item .item-img{
+    width: 100%;
+    height: 0;
+    padding-bottom: 37.1%; 
+}
+.list-item .item-img img{
+    width: 100%;
+}
+
+
+.list-item .item-info{
+    padding: .14rem .2rem .2rem;
+}
+
+.item-info .info-title{
+    width: 100%;
+    height: .48rem;
+    line-height: .48rem;
+    font-size: .28rem;
+    ellipsis();
+}
+
+.item-info .info-comment{
+    width: 100%;
+    height: .42rem;
+    line-height: .28rem;
+    font-size: .24rem;
+    color: $lightTxtColor;
+    ellipsis();
+}
+
+</style>
