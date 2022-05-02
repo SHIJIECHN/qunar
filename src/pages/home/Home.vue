e5fa3ebbffc7d84a4874ced22a1f4dd9e86175d7
shijie <shijiechn@aliyun.com>
2022-05-02 11:48:34 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
 </div>
     
     
@@ -8,10 +9,13 @@
 
 <script>
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
+
 export default {
     name: 'Home',
     components: {
-        'home-header': HomeHeader
+        'home-header': HomeHeader,
+        'home-swiper': HomeSwiper
     }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,73 @@
+<!-- The ref attr used to find the swiper instance -->
+<template>
+<div class="sw-wrapper">
+   <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+  // swiper options example:
+  export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+       swiperOption: {
+         pagination: '.swiper-pagination',
+         loop: true,
+       },
+       swiperList: [
+         {
+           id: '001',
+           imgUrl: 'https://dummyimage.com/750x200/a3a1a3/fff'
+         },
+         {
+           id: '002',
+           imgUrl: 'https://dummyimage.com/750x200/0b7841/fff'
+         },
+         {
+           id: '003',
+           imgUrl: 'https://dummyimage.com/750x200/4f992c/fff'
+         },
+         {
+           id: '004',
+           imgUrl: 'https://dummyimage.com/750x200/992c49/fff'
+         },
+         {
+           id: '005',
+           imgUrl: 'https://dummyimage.com/750x200/99952c/fff'
+         },
+         {
+           id: '006',
+           imgUrl: 'https://dummyimage.com/750x200/99352c/fff'
+         },
+       ]
+        
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.sw-wrapper >>> .swiper-pagination-bullet-active{
+  background-color: #fff;
+}
+
+.sw-wrapper{
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 26.67%;
+  background-color: #eee;
+}
+.sw-wrapper .swiper-img{
+  width: 100%;
+  height: 100%;
+}
+</style>
